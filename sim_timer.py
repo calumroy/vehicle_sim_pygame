e51f1aa4afc718e27fe03650c6fe5668113f9bf7
calumroy <calum@calumroy.com>
2021-05-30 14:48:01 +0800

    Very bad scaling imlemented

--- sim_timer.py
+++ sim_timer.py
@@ -11,6 +11,8 @@ class SimTimer:
         self.averaging_const = 0.05
         self.print_time_period = 1.0
         self.last_print_time = time_now
+        self.reset_period = 10.0
+        self.last_reset_time = time_now
 
     def update(self):
         time_now = time.time()
@@ -28,4 +30,11 @@ class SimTimer:
         if ( time_diff > self.print_time_period):
             print("min = {0} max = {1} average hz = {2}".format(self.min_time, self.max_time, self.average_loop_rate_hz))
             self.last_print_time = time_now
+        # Reset timer.
+        reset_diff = (time_now - self.last_reset_time)
+        if ( reset_diff > self.reset_period):
+            self.max_time = time_diff
+            self.min_time = time_diff
+            self.last_reset_time = time_now
+
         self.last_update_time = time_now
--- vehicle_sim.py
+++ vehicle_sim.py
@@ -21,17 +21,21 @@ RED = (255, 0, 0)
 pygame.init()
  
 FRAMES_PER_SEC = 60
-UPDATE_SIM_HZ = 20
+UPDATE_SIM_HZ = 5
 update_sim_period = 1.0 / float(UPDATE_SIM_HZ)
 frames_per_sim_update = int((1.0 / float(UPDATE_SIM_HZ)) * FRAMES_PER_SEC)
 print("frames_per_sim_update = {0}".format(frames_per_sim_update))
 
 # Set the width and height of the screen [width, height]
-size = (2*700, 2*500)
-screen = pygame.display.set_mode(size)
+display_size = (800, 600)
+screen = pygame.display.set_mode(display_size)
 pixel_to_meters_scale = 40.0  # The number of pixels per simulated meter. 
 newtons_per_key_press = 200.0
 rads_per_key_press = 0.02
+zoom = 1
+zoom_factor = 0.05
+n_anti_aliasing_ratio = 2
+border_radius = 10
  
 pygame.display.set_caption("Vehicle Sim")
  
@@ -55,21 +59,20 @@ print_time_period = 1.0 / 2.0
 game_timer = simtimer.SimTimer()
 
 # Start at a negative y position since the screens top left pos is (0,0)
-start_pos = (float(size[0]/2.0),-1.0*float(size[1]/2.0))
+start_pos = (float(display_size[0]/2.0),-1.0*float(display_size[1]/2.0))
 # Convert the start pos to coordinates for the vehicle
 start_pos = (start_pos[0] / pixel_to_meters_scale, start_pos[1] / pixel_to_meters_scale)
 car = mi.Vehicle(start_pos) 
 
 def print_car_state(car):
-    car.control_input["Fx"]= (keys["up"] - keys["down"]) * newtons_per_key_press
-    car.control_input["delta"] = (keys["left"] - keys["right"]) * rads_per_key_press
-    print("  Vehicle input controls: \n\t\t Fx = {0} \n\t\t delta = {1}".format(car.control_input["Fx"], car.control_input["delta"]))
-    print(" car.state[x] = {0}".format(car.state["x"]))
-    print(" car.state[y] = {0}".format(car.state["y"]))
-    print(" car.state[phi] = {0}".format(car.state["phi"]))
-    print(" car.state[vx] = {0}".format(car.state["vx"]))
-    print(" car.state[vy] = {0}".format(car.state["vy"]))
-    print(" car.state[r] = {0}".format(car.state["r"]))
+    pass
+    # print("  Vehicle input controls: \n\t\t Fx = {0} \n\t\t delta = {1}".format(car.control_input["Fx"], car.control_input["delta"]))
+    # print(" car.state[x] = {0}".format(car.state["x"]))
+    # print(" car.state[y] = {0}".format(car.state["y"]))
+    # print(" car.state[phi] = {0}".format(car.state["phi"]))
+    # print(" car.state[vx] = {0}".format(car.state["vx"]))
+    # print(" car.state[vy] = {0}".format(car.state["vy"]))
+    # print(" car.state[r] = {0}".format(car.state["r"]))
 
 def update(car, update_sim_period):
     global done
@@ -98,7 +101,7 @@ def rectRotated( surface, color, pos, fill, border_radius, rotation_angle, rotat
     rw2=pos[2]//2 # halfwidth of rectangle
     rh2=pos[3]//2
 
-    pygame.draw.rect( s, color, ((surfcenterx-rw2-rotation_offset_center[0])*nRenderRatio,(surfcentery-rh2-rotation_offset_center[1])*nRenderRatio,pos[2]*nRenderRatio,pos[3]*nRenderRatio), fill*nRenderRatio, border_radius=border_radius*nRenderRatio )
+    pygame.draw.rect( s, color, ((surfcenterx-rw2-rotation_offset_center[0])*nRenderRatio,(surfcentery-rh2-rotation_offset_center[1])*nRenderRatio,pos[2]*nRenderRatio,pos[3]*nRenderRatio), (not fill)*nRenderRatio, border_radius=border_radius*nRenderRatio )
     s = pygame.transform.rotate( s, rotation_angle )        
     if nRenderRatio != 1: s = pygame.transform.smoothscale(s,(s.get_width()//nRenderRatio,s.get_height()//nRenderRatio))
     incfromrotw = (s.get_width()-sw)//2
@@ -127,9 +130,38 @@ while not done:
                 keys["left"] -= 1.0
             elif event.key == pygame.K_q:
                 done = True
+            elif event.key == pygame.K_0:
+                zoom = 2
+                print('RESET')
+        # Zoom in out functions
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 4: # wheel rolled up
+            zoom += zoom_factor
+            print('ZOOMING IN')
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 5: # wheel rolled down
+            zoom -= zoom_factor
+            print('ZOOMING OUT')
 
  
     # --- Game logic should go here
+
+    # Update control inputs
+    car.control_input["Fx"]= (keys["up"] - keys["down"]) * newtons_per_key_press
+    car.control_input["delta"] = (keys["left"] - keys["right"]) * rads_per_key_press
+    
+    # Run simulation code
+    tick_count += 1
+    if tick_count > frames_per_sim_update:
+        tick_count = 0
+        update(car, update_sim_period)
+        # Add timer to keep track of how quickly the sim is running.
+        game_timer.update()
+
+    # Print out functions.
+    time_now = time.time()
+    time_diff = (time_now - last_print_time)
+    if ( time_diff > print_time_period):
+        print_car_state(car)
+        last_print_time = time_now
  
     # --- Screen-clearing code goes here
  
@@ -138,37 +170,33 @@ while not done:
  
     # If you want a background image, replace this clear with blit'ing the
     # background image.
-    screen.fill(WHITE)
+    #screen.fill(WHITE)
 
     # --- Drawing code should go here
+    display = pygame.Surface((int(display_size[0] / zoom), int(display_size[1] / zoom)))
+    display.fill(WHITE)
+
     fill = True
-    border_radius = 2
     rotation_angle = car.state["phi"] * 180.0 / math.pi  # Convert from radians to degrees.
     # Negate the y positions since pygame uses downwards as positive y direction.
-    rect_state = (car.state["x"] * pixel_to_meters_scale,-1.0*car.state["y"] * pixel_to_meters_scale,
+    rect_x = car.state["x"] * pixel_to_meters_scale
+    rect_y = -1.0*car.state["y"] * pixel_to_meters_scale
+    rect_state = (rect_x, rect_y,
                   car.params["car_l"]*pixel_to_meters_scale,
                   car.params["car_w"]*pixel_to_meters_scale)
     angle_offset = (0.0, 0.0)
-    rectRotated( screen, RED, rect_state, fill, border_radius, rotation_angle, angle_offset, 2)
+    rectRotated( display, RED, rect_state, fill, border_radius, rotation_angle, angle_offset, n_anti_aliasing_ratio)
     
-    tick_count += 1
-    if tick_count > frames_per_sim_update:
-        tick_count = 0
-        update(car, update_sim_period)
     # --- Go ahead and update the screen with what we've drawn.
+    screen.blit(pygame.transform.scale(display,display_size),(0,0))
     pygame.display.flip()
  
-    # Print out functions.
-    time_now = time.time()
-    time_diff = (time_now - last_print_time)
-    if ( time_diff > print_time_period):
-        print_car_state(car)
-        last_print_time = time_now
+ 
 
     # --- Limit to FRAMES_PER_SEC frames per second
     clock.tick(FRAMES_PER_SEC)
 
-    game_timer.update()
+    
  
 # Close the window and quit.
 pygame.quit()
